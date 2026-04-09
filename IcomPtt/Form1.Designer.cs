1fa2d419b2fd4bfb5c635a185368bec46b301fe4
kd2grs <59626241+kd2grs@users.noreply.github.com>
2026-04-09 15:52:36 -0400

    Initial Upload / Release

---
+++ IcomPtt/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace IcomPtt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPortIn = new System.IO.Ports.SerialPort(this.components);
+            this.serialPortOut = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // serialPortIn
+            // 
+            this.serialPortIn.PinChanged += new System.IO.Ports.SerialPinChangedEventHandler(this.serialPortIn_PinChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Control Port";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Radio CAT Port";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(26, 111);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(191, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(0, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(526, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Transmit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button2_KeyDown);
+            this.button2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button2_KeyUp);
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(26, 142);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(191, 24);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(26, 173);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(191, 24);
+            this.comboBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 253);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "icom PTT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPortIn;
+        private System.IO.Ports.SerialPort serialPortOut;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ IcomPtt/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace IcomPtt
+{
+    public partial class Form1 : Form
+    {
+        bool xmit = false;
+        byte[] bytesxmit;
+        byte[] bytesrecv;
+        Properties.Settings appSettings;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void serialPortIn_PinChanged(object sender, System.IO.Ports.SerialPinChangedEventArgs e)
+        {
+            if (e.EventType == SerialPinChange.CtsChanged)
+            {
+                CheckPtt();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            appSettings = new Properties.Settings();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string portName in ports)
+            {
+                comboBox1.Items.Add(portName);
+                comboBox2.Items.Add(portName);
+            }
+            comboBox3.Items.Add("57600");
+            comboBox3.Items.Add("38400");
+            comboBox3.Items.Add("19200");
+            comboBox3.Items.Add("9600");
+            comboBox3.Items.Add("4800");
+            comboBox3.Items.Add("2400");
+            comboBox3.Items.Add("1200");
+
+            comboBox4.Items.Add("88"); //IC-7100
+            comboBox4.Items.Add("76"); //IC-7200
+            comboBox4.Items.Add("7C"); //IC-9100
+
+            comboBox1.SelectedItem = appSettings.ControlPort;
+            comboBox2.SelectedItem = appSettings.RadioPort;
+            comboBox3.SelectedItem = appSettings.RadioPortBuad;
+            comboBox4.SelectedItem = appSettings.RadioAddr;
+
+            this.label1.Text = "PTT off";
+        }
+
+        private void CheckPtt()
+        {
+            if (serialPortIn.IsOpen & serialPortOut.IsOpen)
+            {
+                if ((serialPortIn.CtsHolding == true) | this.xmit)
+                {
+                    serialPortOut.Write(this.bytesxmit, 0, 8);
+                    label1.Text = "PTT on";
+                }
+                else
+                {
+                    serialPortOut.Write(this.bytesrecv, 0, 8);
+                    label1.Text = "PTT off";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Serial ports are closed, please check settings and click Open.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPortIn.IsOpen)
+            {
+                serialPortIn.Close();
+            }
+            serialPortIn.PortName = comboBox1.SelectedItem.ToString();
+            serialPortIn.Open();
+
+            if (serialPortOut.IsOpen)
+            {
+                serialPortOut.Close();
+            }
+            serialPortOut.PortName = comboBox2.SelectedItem.ToString();
+            serialPortOut.BaudRate = Convert.ToInt32(comboBox3.SelectedItem);
+            serialPortOut.DataBits = 8;
+            serialPortOut.StopBits = StopBits.One;
+            serialPortOut.Parity = Parity.None;
+            serialPortOut.Open();
+
+            this.UpdateLabels();
+            this.CreateMessages();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateLabels()
+        {
+            if (this.serialPortIn.IsOpen)
+            {
+                this.label2.Text = "Control Port (open)";
+            }
+            else
+            {
+                this.label2.Text = "Control Port";
+            }
+
+            if (this.serialPortOut.IsOpen)
+            {
+                this.label3.Text = "Radio CAT Port (open)";
+            }
+            else
+            {
+                this.label3.Text = "Radio CAT Port";
+            }
+        }
+
+        private void CreateMessages()
+        {
+            //Transmit command string;
+            bytesxmit = new byte[10];
+            bytesxmit[0] = 254;
+            bytesxmit[1] = 254;
+            bytesxmit[2] = Convert.ToByte(comboBox4.SelectedItem.ToString(), 16); //0x88;
+            bytesxmit[3] = 224;
+            bytesxmit[4] = 28;
+            bytesxmit[5] = 0;
+            bytesxmit[6] = 1;
+            bytesxmit[7] = 253;
+
+            //Recieve command string;
+            bytesrecv = new byte[10];
+            bytesrecv[0] = 254;
+            bytesrecv[1] = 254;
+            bytesrecv[2] = Convert.ToByte(comboBox4.SelectedItem.ToString(), 16); //0x88; 
+            bytesrecv[3] = 224;
+            bytesrecv[4] = 28;
+            bytesrecv[5] = 0;
+            bytesrecv[6] = 0;
+            bytesrecv[7] = 253;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            xmit = true;
+            this.CheckPtt();
+        }
+
+        private void button2_KeyUp(object sender, KeyEventArgs e)
+        {
+            xmit = false;
+            this.CheckPtt();
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            xmit = true;
+            this.CheckPtt();
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            xmit = false;
+            this.CheckPtt();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Close input port to stop / prevent more triggering
+            serialPortIn.Close();
+
+            //If radio COM port is open, transmit the sequence to stop transmitting (just in case), then close it too.
+            if (serialPortOut.IsOpen)
+            {
+                serialPortOut.Write(this.bytesrecv, 0, 8);
+                serialPortOut.Close();
+            }
+
+            //Now normal closing of form proceeds.
+        }
+    }
+}
